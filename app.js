81a71ca7908e90577a4d3f79e03127856ea69a71
RayanEreteo <rayabf5@gmail.com>
2022-09-19 20:22:00 +0200

    finished

--- app.js
+++ app.js
@@ -5,20 +5,25 @@ $(document).ready(function(){
 
     let buttonStart = $('#start-test')
     let core = $(".main-container");
+
     let text = $(".tutorial");
     let waitText = $(core).children('.wait-text');
     let roundText = $('.round');
+    let remainingTime = $(".remaning-time");
 
     let currentRound = 1;
+    let speed = 390;
 
     let isScreenGreen = false;
     let testStarted = false;
 
     $(buttonStart).click(startTest);
     $(core).click(checkStateOfScreen);
-    $(roundText).text("PREPARE FOR ROUND " + currentRound + " ...");
+    $(roundText).text("ROUND " + currentRound + " ...");
+    $(remainingTime).text("time left to react : " + speed + "ms");
 
     function startTest(e){
+        $(core).css("background", "#00aeff");
         e.stopPropagation();
         testStarted = true;
         $(text).text('Click anywhere as fast as possible when the screen turn green.');
@@ -31,7 +36,7 @@ $(document).ready(function(){
         if (testStarted && !isScreenGreen) {
             tooSoon();
         }else if(testStarted && isScreenGreen){
-            testResult();
+            RoundPassed();
         }
     }
 
@@ -40,23 +45,61 @@ $(document).ready(function(){
         $(waitText).hide();
         $(core).css("cursor", "pointer");
         $(core).css('background', '#33cc00');
-        setInterval(recordMS, 10);
+        windowInterval = setTimeout(tooLate, speed);
     }
 
     function tooSoon(){
+        currentRound = 1
+
         $(core.children()).show();
         $(waitText).hide();
-        $(text).text("TEST FAILED - Don't click before the screen turn green")
+        $(text).text("TEST FAILED - CLICKED TOO SOON")
+        $(roundText).text("ROUND " + currentRound + " ...");
+
         clearTimeout(randomTime);
+
+        speed = 390;
+        $(remainingTime).text("time left to react : " + speed + "ms");
     }
 
-    function testResult(){
+    function tooLate(){
+        currentRound = 1;
+
         testStarted = false;
         isScreenGreen = false;
+
+        $(core).css("cursor", "default");
+        $(core.children()).show();
+        $(core).css("background", "#ff0000");
+        $(waitText).hide();
+        $(text).text("TOO LATE, TRY AGAIN");
+        $(roundText).text("ROUND " + currentRound + " ...");
+
+        clearTimeout(randomTime);
+
+        speed = 390;
+        $(remainingTime).text("time left to react : " + speed + "ms");
+    }
+
+    function RoundPassed(){
+        testStarted = false;
+        isScreenGreen = false;
+
+        currentRound++;
+
+        $(core).css("cursor", "default");
         $(core).css("background", "#00aeff");
-        $(text).text("Success");
+        $(text).text("Round passed ! Speed will increase.");
         $(core.children().show())
         $(waitText).hide();
+        $(roundText).text("ROUND " + currentRound + " ...");
+        
+        speed -= 30;
+        $(remainingTime).text("time left to react : " + speed + "ms");
+
         clearTimeout(randomTime);
+        clearTimeout(windowInterval);
+
+        console.log(speed)
     }
 })
