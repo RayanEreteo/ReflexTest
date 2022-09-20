d7d323ba20ebc200fdac492ccca6f9565fcf729e
RayanEreteo <rayabf5@gmail.com>
2022-09-20 19:47:30 +0200

    finished project

--- app.js
+++ app.js
@@ -1,53 +1,64 @@
-$(document).ready(function(){
-    window.addEventListener('contextmenu', function(e){
-        e.preventDefault();
-    })
+//UPPERCASE COMMENT --- Category
+//lowercase comment --- Line explanation
 
+// Check if document is fully loaded
+$(document).ready(function(){
+    //VARIABLES
+    // Important element declaration
     let buttonStart = $('#start-test')
     let core = $(".main-container");
 
     let text = $(".tutorial");
     let waitText = $(core).children('.wait-text');
     let roundText = $('.round');
-    let remainingTime = $(".remaning-time");
-
+    let remainingTime = $(".remaining-time");
+    // Declare number of rounds
     let currentRound = 1;
+    // Declare the speed in ms
     let speed = 390;
-
+    // Declare boolean
     let isScreenGreen = false;
     let testStarted = false;
-
+    // LISTENERS
+    // Start the test after clicking the button
     $(buttonStart).click(startTest);
     $(core).click(checkStateOfScreen);
+    //Set the text of these elements with the variable above
     $(roundText).text("ROUND " + currentRound + " ...");
     $(remainingTime).text("time left to react : " + speed + "ms");
-
+    // FUNCTIONS
+    // Function to start the test
     function startTest(e){
         $(core).css("background", "#00aeff");
+        // Stop propagation of the click
         e.stopPropagation();
         testStarted = true;
         $(text).text('Click anywhere as fast as possible when the screen turn green.');
         $(core.children()).hide();
         $(waitText).show();
+        // Set a timer with an ID of "randomTime" with random time between 1 and 5 seconds and call the isGreen function
         randomTime = setTimeout(isGreen, Math.floor(Math.random() * 5000));
     }
-
+    // Function to check if the screen background is green after we cicked it 
     function checkStateOfScreen(e){
+        // Check if the test started and the screen is NOT green
         if (testStarted && !isScreenGreen) {
             tooSoon();
+            // Check if the test started and the screen is green
         }else if(testStarted && isScreenGreen){
             RoundPassed();
         }
     }
-
+    // Do some changes if the screen is green
     function isGreen(){
         isScreenGreen = true;
         $(waitText).hide();
         $(core).css("cursor", "pointer");
         $(core).css('background', '#33cc00');
+        // Set a timer with an ID of "windowInterval" with the variable 'speed' as miliseconds and will be the time left to click before failing the test 
         windowInterval = setTimeout(tooLate, speed);
     }
-
+    // Stop the test if we clicked before screen turned green
     function tooSoon(){
         currentRound = 1
 
@@ -61,7 +72,7 @@ $(document).ready(function(){
         speed = 390;
         $(remainingTime).text("time left to react : " + speed + "ms");
     }
-
+    // Stop the test if it's too late (depend on the speed variable in milliseconds wich is 390ms by default)
     function tooLate(){
         currentRound = 1;
 
@@ -80,7 +91,7 @@ $(document).ready(function(){
         speed = 390;
         $(remainingTime).text("time left to react : " + speed + "ms");
     }
-
+    // Function if we win the round 
     function RoundPassed(){
         testStarted = false;
         isScreenGreen = false;
@@ -99,7 +110,5 @@ $(document).ready(function(){
 
         clearTimeout(randomTime);
         clearTimeout(windowInterval);
-
-        console.log(speed)
     }
 })
